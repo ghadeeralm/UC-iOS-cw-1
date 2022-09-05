11097bef6af49448babe4d56c7b71d657a15ffd4
ghadeeralm <ghaderalm@outlook.com>
2022-09-05 19:39:16 +0300

    empty

---
+++ cw1-ghadeer/cw1-ghadeer/cw1_ghadeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  cw1_ghadeerApp.swift
+//  cw1-ghadeer
+//
+//  Created by ghadeer almajed on 05/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct cw1_ghadeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
